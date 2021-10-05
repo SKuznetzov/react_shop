ed506a55f6546c2f20c53f79eb752f2dd89a2b6e
SKuznetzov <s-kuznecov100@rambler.ru>
2021-10-05 21:21:34 +0300

    start commit

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React,{Component} from 'react';
+
+class App extends Component {
+    render(){
+        return <p>Hello App</p>
+    }
+}
+ export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('Hello');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(<App />,document.querySelector('#root'));
+
+
