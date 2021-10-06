677ca8930e0ced5c3cda45520164655c8563e96f
SKuznetzov <s-kuznecov100@rambler.ru>
2021-10-06 19:15:05 +0300

    add first page

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,31 @@
 import React,{Component} from 'react';
+import restaurants from '../sample-restaurants';
+
 
 class App extends Component {
     render(){
-        return <p>Hello App</p>
+
+        return (
+            <div className='restorant_select'>
+               <div className='restorant_select_top'>
+                  <div className='restorant_select_top-header'>Выбери ресторан</div>
+                   
+                  <div className='arrow_picker'>
+                  <div className='arrow_picker_up'></div>
+                  <div className='arrow_picker_down'></div>
+                </div>
+              </div>
+
+              <div className='restaurant_select_button'>
+                  <ul>
+                      {restaurants.map(restaurant =>{
+                       return <li key={restaurant.id}>{restaurant.title}</li>
+                      })}
+                  </ul>
+              </div>
+              <button>Перейти в ресторан</button>
+            </div>
+        );
     }
 }
  export default App;
