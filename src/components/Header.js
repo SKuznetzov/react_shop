3cb5b982fbede7567173551acc26f616ff521aed
SKuznetzov <s-kuznecov100@rambler.ru>
2021-10-09 21:26:01 +0300

    add function Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,29 @@
 import React, { Component } from 'react'
 
-export default class Header extends Component {
-    render() {
-        return (
-            <header className="top">
-                <div className="wrap">
-                    <div className="header-content">
+function Header(props){
+    return (
+        <header className="top">
+            <div className="wrap">
+                <div className="header-content">
 
-                        <div className="header-rating">
-                            <div className="header-rating">Рейтинг: </div>
-                            <div className="header-rating">★★★★★</div>
-                        </div>
-
-                        <div className="header-divider"></div>
-                        <h1 className="font-effect-fire-animation">{this.props.title}</h1>
-                        <h3>
-                            <span>
-                             Быстрая доставка горячих
-                             <span className="sub-header"></span>
-                            </span>
-                        </h3>
+                    <div className="header-rating">
+                        <div className="header-rating">Рейтинг: </div>
+                        <div className="header-rating">★★★★★</div>
                     </div>
-                </div>
 
-            </header>
-        )
-    }
+                    <div className="header-divider"></div>
+                    <h1 className="font-effect-fire-animation">{props.title}</h1>
+                    <h3>
+                        <span>
+                         Быстрая доставка горячих
+                         <span className="sub-header"></span>
+                        </span>
+                    </h3>
+                </div>
+            </div>
+
+        </header>
+    )
 }
+export default Header;
+
