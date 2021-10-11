c211beae649ad28e689b778ab076ebbc615bb912
SKuznetzov <s-kuznecov100@rambler.ru>
2021-10-11 16:49:08 +0300

    add burger,ref,function createBurger

--- src/components/AddBurgerForm.js
+++ src/components/AddBurgerForm.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react'
 
 export default class AddBurgerForm extends Component {
+    nameRef = React.createRef()
+    priceRef = React.createRef()
+    statusRef = React.createRef()
+    descRef = React.createRef()
+    imageRef = React.createRef()
+
+    createBurger = (event) =>{
+        event.preventDefault()
+        const burger = {
+            name: this.nameRef.current.value,
+            price: parseFloat(this.praceRef.current.value),
+            status: this.statusRef.current.value,
+            desc: this.descRef.current.value,
+            image: this.imageRef.current.value,
+        }
+    }
     render() {
         return (
-            <form className="burger-edit">
-                <input name="name" type="text" placeholder="Name" autoComplete="off" />
-                <input name="price" type="text" placeholder="Price" autoComplete="off" />
-                <select name="status" className="status">
+            <form className="burger-edit" onSubmit={this.createBurger}>
+                <input ref={this.nameRef} name="name" type="text" placeholder="Name" autoComplete="off" />
+                <input ref={this.priceRef} name="price" type="text" placeholder="Price" autoComplete="off" />
+                <select ref={this.statusRef} name="status" className="status">
                     <option value="available">Доступно</option>
                     <option value="unavailable">Убрать из меню</option>
                 </select>
-                <textarea name="desc"  placeholder="Desc"  />
-                <input name="image" type="text" placeholder="Image" autoComplete="off" />
+                <textarea ref={this.descRef} name="desc"  placeholder="Desc"  />
+                <input ref={this.imageRef} name="image" type="text" placeholder="Image" autoComplete="off" />
+                <button type="submit">Добавить в Меню</button>
             </form>
         )
     }
