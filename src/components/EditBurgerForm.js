0c0c9fd67e1629e678883f4458341a4ff3140d4e
SKuznetzov <s-kuznecov100@rambler.ru>
2021-10-16 10:50:05 +0300

    create a table of properties

--- src/components/EditBurgerForm.js
+++ src/components/EditBurgerForm.js
@@ -4,7 +4,14 @@ export default class EditBurgerForm extends Component {
     render() {
         return (
             <div className="burger-edit">
-                <input />
+                <input name='name' type='text' />
+                <input name='price' type='text' />
+                <select name='status' className='status'>
+                    <option value='available'>Доступно</option>
+                    <option value='unavailable'>Неоступно</option>
+                </select>
+                <textarea name='desc' />
+                <input name='image' type='text' />
             </div>
         )
     }
