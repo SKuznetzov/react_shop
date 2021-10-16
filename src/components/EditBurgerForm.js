e2e47de54b2a8d117b0b3d3927292adfbb381876
SKuznetzov <s-kuznecov100@rambler.ru>
2021-10-16 10:54:58 +0300

    fill in the table

--- src/components/EditBurgerForm.js
+++ src/components/EditBurgerForm.js
@@ -4,14 +4,14 @@ export default class EditBurgerForm extends Component {
     render() {
         return (
             <div className="burger-edit">
-                <input name='name' type='text' />
-                <input name='price' type='text' />
-                <select name='status' className='status'>
+                <input name='name' type='text' value={this.props.burger.name } />
+                <input name='price' type='text' value={this.props.burger.price} />
+                <select name='status' className='status'value={this.props.burger.status} >
                     <option value='available'>Доступно</option>
                     <option value='unavailable'>Неоступно</option>
                 </select>
-                <textarea name='desc' />
-                <input name='image' type='text' />
+                <textarea name='desc' value={this.props.burger.desc} />
+                <input name='image' type='text' value={this.props.burger.image} />
             </div>
         )
     }
