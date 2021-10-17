de079cbc66d9e80b98ef677c20d0f04ae11acc4b
SKuznetzov <s-kuznecov100@rambler.ru>
2021-10-17 15:32:59 +0300

    import auth

--- src/Base.js
+++ src/Base.js
@@ -1,6 +1,8 @@
 import Rebase from 're-base'
 import firebase from 'firebase/app'
-require('firebase/database')
+import 'firebase/database'
+import 'firebase/auth'
+
 
 const firebaseApp = firebase.initializeApp({
     apiKey: "AIzaSyBPEaGEPXW6rV_FzM-ZZAhSxgrjlO9H8SQ",
